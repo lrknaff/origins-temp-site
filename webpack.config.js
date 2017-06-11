494fd2380a45b7e02bd4f0a895646b9f64850712
lrknaff <lacey.knaff@gmail.com>
2017-06-11 15:45:50 -0600

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
-}
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+//
+// const PATHS = {
+//   app: path.join(__dirname, 'lib'),
+//   build: path.join(__dirname, 'build')
+// }
 
 module.exports = {
   entry: {
-    main: PATHS.app + '/index.js'
+    main: './lib/index.js'
   },
   output: {
-    path: PATHS.build,
+    path: __dirname,
     filename: '[name].bundle.js'
   },
   module: {
-    loaders: [
-      { test: /\.css$/, loader: "style!css" },
-      { test: /\.scss$/, loader: "style!css!sass" },
-      { test: /\.jsx?$/, exclude: '/node_modules/', loader: 'babel-loader' }
+    rules: [
+      // { test: /\.jsx?$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.css$/, loader: "css-loader" },
+      { test: /\.scss$/, use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }] }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: PATHS.build + '/index.html',
-      title: 'React In Theory',
-      inject: 'body'
-    })
-  ],
-  resolve: {
-    extensions: ['', '.scss', '.css', '.js', '.json', '.jsx']
   }
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: PATHS.build + 'index.html',
+  //     title: 'Origin',
+  //     inject: 'body'
+  //   })
+  // ],
+  // resolve: {
+  //   extensions: ['', '.js', '.json', '.scss', 'css']
+  // }
 }
